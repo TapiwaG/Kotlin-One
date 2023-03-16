fef3f94cc66f95c2efb41f1c6ad00a3c0fa1baa4
TapiwaG <Tapiwanashegumbo24@gmail.com>
2023-03-16 10:44:06 +0300

    added other files

---
+++ src/main/kotlin/MethodOverloading.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    avg(34, 424.0)
+}
+
+fun avg(x:Int, y:Double){
+    var answer = (x + y) / 3.0
+    println(answer)
+}
+
+fun avg(x:Int, y:Double, z:Double){
+    var answer = (x + y + z) / 3.0
+    println("Result from avg2 is $answer")
+}
